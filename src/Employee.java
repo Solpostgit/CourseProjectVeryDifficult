6f11d5aab5333fe2a01556afed1dac49cee3b717
Solpostgit <begemot_off@mail.ru>
2023-09-16 16:23:34 +0300

    Первый элемент хранилища

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,56 @@
 public class Employee {
+    static int count = 1;
+    private int id;
+    private String surname;
+    private String name;
+    private String patronymic;
+    private int department;
+    private int salary;
+
+    public Employee(String surname, String name, String patronymic, int department, int salary) {
+
+        this.id = count;
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.department = department;
+        this.salary = salary;
+        count++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "ID:" + this.id + " " + this.surname + " " + this.name + " " + this.patronymic + " " + " Отдел " + this.department + " Зарплата " + this.salary;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employees = new Employee[10]; //поле типа Employee[10]
+
+        Employee employee1 = new Employee("Ivanov", "Ivan", "Ivanovich", 3, 91000);
+        Employee employee2 = new Employee("Petrov", "Petr", "Petrovich", 2, 100000);
+        Employee employee3 = new Employee("Sidorov", "Denis", "Sergeevich", 4, 95000);
+        Employee employee4 = new Employee("Ivanova", "Anna", "Borisovna", 1, 93000);
+        Employee employee5 = new Employee("Svetlaya", "Irina", "Pavlovna", 5, 90000);
+        Employee employee6 = new Employee("Borisov", "Maksim", "Alekseevich", 3, 97000);
+        Employee employee7 = new Employee("Alekseev", "Sergey", "Nikolaevich", 1, 92000);
+        Employee employee8 = new Employee("Maksimova", "Alla", "Nikolaevna", 2, 94000);
+        Employee employee9 = new Employee("Nikolaeva", "Olga", "Anatolyevna", 4, 91000);
+        Employee employee10 = new Employee("Denisov", "Pavel", "Ivanovich", 3, 98000);
+
+        employees[0] = employee1;
+        System.out.println(employees[0]);
     }
 }
